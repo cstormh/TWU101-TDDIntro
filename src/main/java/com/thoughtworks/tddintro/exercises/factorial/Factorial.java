db42b2827172ceae1a7d1b60ffce8c3f6aa47e5f
Storm Heidinger <cstormheidinger@gmail.com>
2018-11-30 15:04:56 -0600

    Passed IllegalArgumentException test in FactorialTests

--- src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
@@ -2,8 +2,12 @@ package com.thoughtworks.tddintro.exercises.factorial;
 
 public class Factorial {
     public Integer compute(int i) {
+
         if (i == 0) {
             return 1;
+        }
+        else if (i < 0) {
+            throw new IllegalArgumentException();
         }
         else {
 
