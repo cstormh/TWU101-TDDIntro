c7f6a1cb106f67e269dbb20200b9d202677ed6c4
Storm Heidinger <cstormheidinger@gmail.com>
2018-11-30 14:55:41 -0600

    Passed first 4 tests in FactorialTests

--- src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
@@ -2,6 +2,19 @@ package com.thoughtworks.tddintro.exercises.factorial;
 
 public class Factorial {
     public Integer compute(int i) {
-        return -1;
+        if (i == 0) {
+            return 1;
+        }
+        else {
+
+            int factorial = i;
+
+            for (int j = 1; j < i; j++) {
+                int n = i - j;
+                factorial *= n;
+            }
+
+            return factorial;
+        }
     }
 }
